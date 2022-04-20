b41720d2949f9f8ff65bb6cca5d15f9346ebd8b9
tcmalware <genarodvo@gmail.com>
2022-04-20 15:31:00 -0500

    Agregar archivos de proyecto.

---
+++ Ejercicio03/Ejercicio03/EntryDemo.xaml.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Ejercicio03
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class EntryDemo : ContentPage
+    {
+        public EntryDemo()
+        {
+            InitializeComponent();
+
+            StackLayout stack = new StackLayout();
+            var entry = new Entry { Text = "This is a read-only Entry" };
+            stack.Children.Add(entry);
+            Content = stack;
+        }
+    }
+}
